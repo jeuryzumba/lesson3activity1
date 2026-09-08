d13facbd43dc8771b2b89cf2d9432a5c69e836eb
jeuryzumba <jeuryzumba9@gmail.com>
2026-09-07 21:48:48 -0400

    string operations

---
+++ activity_3.py
@@ -0,0 +1,8 @@
+str1 = "Hello"
+str2 = "world"
+str3 = str1 + str2
+print (str3.upper())
+txt = input("enter your sentence: \n")
+print("after reversing the sentence")
+revText = txt[::-1]
+print(revText)
