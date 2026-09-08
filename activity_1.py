3b0190c564ff5fe46e7b38e424000ba8f2124a73
jeuryzumba <jeuryzumba9@gmail.com>
2026-09-07 21:02:47 -0400

    Activity1 Learn about Data types

---
+++ activity_1.py
@@ -0,0 +1,15 @@
+print("jeury")
+
+food = "pizza"
+print("type of food:", type(food))
+
+a = 20
+print("type of a:", type(a))
+
+b = 12.4
+
+print("type of b:", type(b))
+
+c = False
+
+print("type of c:", type(c))
