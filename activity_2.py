da1f2b6a9f5c850d57e98ac0239417771f9271d1
jeuryzumba <jeuryzumba9@gmail.com>
2026-09-07 21:25:54 -0400

    type casting

---
+++ activity_2.py
@@ -0,0 +1,17 @@
+name = "jeury"
+age = 14
+is_student = True
+weight = 110.5
+print("Name:", name)
+print("type of name:", type(name))
+print("Age:", age)
+print("type of age:", type(age))
+print("Is Student:", is_student)
+print("type of is_student:", type(is_student))
+print("Weight:", weight)
+print("type of weight:", type(weight))
+print("after type casting")
+age = str(age)
+print("type of age:", type(age))
+weight = int(weight)
+print("type of weight:", type(weight))
